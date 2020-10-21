64fcdecbd3ca07490f8cfbb09990b3eb565c8365
pujiermanto <pujiermanto@gmail.com>
2020-10-21 18:34:21 +0700

    Create callFunc.js

---
+++ js/callFunc.js
@@ -0,0 +1,14 @@
+// // PT. Syoobe Inti Perdana | 2020
+// // Community Syoobe Indonesia
+
+
+// let date = new Date();
+// let year = date.getFullYear();
+// let element = `<div class="copyright">Copyright &copy; ${year} <br/> <a href="https://blog.syoobe.co.id/" title="Syoobe Blog" ><span>Community Syoobe Indonesia | </span></a> <a href="https://syoobe.co.id/"> All rights reserved</a>.</div>`;
+// copyright(element)
+// let child = '.footer-socket-left-section';
+// FooterSocketLeft(child);
+
+
+baseUrl('https://blog.syoobe.co.id/');
+
